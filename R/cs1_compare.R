9f1203e5200fd6e9dbeb5a90c4fbf15f7f557527
pascoalf <up202010522@g.uporto.pt>
2026-03-05 09:27:38 +0000

    Function for alternative metrics - flat threshold

--- R/cs1_compare.R
+++ R/cs1_compare.R
@@ -1,18 +1,5 @@
 # CS1 compare alternatives
 
-## REDO --> use transactions object ---> on top of it calculate mutual info and improvement
-
-
-# Just Confidence 90%
-conf.90_all <- full_rules[quality(full_rules)$confidence >= 0.9]
-quality(conf.90_all)$mutualInfo <- interestMeasure(conf.90_all, measure = "mutualInformation")
-quality(conf.90_all)$improvement <- interestMeasure(conf.90_all, measure = "improvement")
-
-conf.90_dep <- full_rules_dependent[quality(full_rules_dependent)$confidence >= 0.9]
-quality(conf.90_dep)$mutualInfo <- interestMeasure(conf.90_dep, measure = "mutualInformation")
-quality(conf.90_dep)$improvement <- interestMeasure(conf.90_dep, measure = "improvement")
-
-
 ## function to get rules by approach
 get_rules_by <- function(rules, by = NULL, metric = "confidence", score = NULL){
   if(metric == "confidence"){
@@ -26,74 +13,16 @@ get_rules_by <- function(rules, by = NULL, metric = "confidence", score = NULL){
   quality(temp_rules)$improvement <- interestMeasure(temp_rules, measure = "improvement")
   return(temp_rules)
 }
-test <- get_rules_by(full_rules_dependent, metric = "lift", score = 10)
 
-## how to implement this for arranged rules??
+# Flat threshold approach
+conf.90_all  <- get_rules_by(full_rules, metric = "confidence", score = 0.9)
+conf.90_dep <- get_rules_by(full_rules_dependent, metric = "confidence", score = 0.9)
+conf.100_all <- get_rules_by(full_rules, metric = "confidence", score = 1)
+conf.100_dep <- get_rules_by(full_rules_dependent, metric = "confidence", score = 1)
+lift.1_all <- get_rules_by(full_rules, metric = "lift", score = 1)
+lift.1_dep <- get_rules_by(full_rules_dependent, metric = "lift", score = 1)
 
-
-# Just Confidence 100%
-conf.100_all <- full_rules_df %>% filter(confidence == 1) %>% mutate(Strategy = "Conf.100%", Set = "All")
-conf.100_dep <- full_rules_df_dependent %>% filter(confidence == 1) %>% mutate(Strategy = "Conf.100%", Set = "Dependent")%>% 
-  select(!Dependence)
-
-# Just Lift > 1
-lift.1_all <- full_rules_df %>% filter(lift > 1) %>% mutate(Strategy = "Lift.1", Set = "All")
-lift.1_dep <- full_rules_df_dependent %>% filter(lift > 1) %>% mutate(Strategy = "Lift.1", Set = "Dependent")%>% 
-  select(!Dependence)
-
-# Top 100 by Conf
-top100_conf_all <- full_rules_df %>% arrange(desc(confidence)) %>% head(100) %>% mutate(Strategy = "top100.Conf", Set = "All")
-top100_conf_dep <- full_rules_df_dependent %>% arrange(desc(confidence)) %>% head(100) %>% mutate(Strategy = "top100.Conf", Set = "Dependent")%>% 
-  select(!Dependence)
-
-# Top 10 by Conf
-top10_conf_all <- full_rules_df %>% arrange(desc(confidence)) %>% head(10) %>% mutate(Strategy = "top10.Conf", Set = "All")
-top10_conf_dep <- full_rules_df_dependent %>% arrange(desc(confidence)) %>% head(10) %>% mutate(Strategy = "top10.Conf", Set = "Dependent")%>% 
-  select(!Dependence)
-
-# Top 100 by Conv
-top100_conv_all <- full_rules_df %>% arrange(desc(Conviction)) %>% head(100) %>% mutate(Strategy = "top100.Conv", Set = "All")
-top100_conv_dep <- full_rules_df_dependent %>% arrange(desc(Conviction)) %>% head(100) %>% mutate(Strategy = "top100.Conv", Set = "Dependent")%>% 
-  select(!Dependence)
-
-# Top 10 by Conv
-top10_conv_all <- full_rules_df %>% arrange(desc(Conviction)) %>% head(10) %>% mutate(Strategy = "top10.Conv", Set = "All")
-top10_conv_dep <- full_rules_df_dependent %>% arrange(desc(Conviction)) %>% head(10) %>% mutate(Strategy = "top10.Conv", Set = "Dependent")%>% 
-  select(!Dependence)
-
-# Top 100 by Lift
-top100_lift_all <- full_rules_df %>% arrange(desc(lift)) %>% head(100) %>% mutate(Strategy = "top100.Lift", Set = "All")
-top100_lift_dep <- full_rules_df_dependent %>% arrange(desc(lift)) %>% head(100) %>% mutate(Strategy = "top100.Lift", Set = "Dependent")%>% 
-  select(!Dependence)
-
-# Top 10 by Lift
-top10_lift_all <- full_rules_df %>% arrange(desc(lift)) %>% head(10) %>% mutate(Strategy = "top10.Lift", Set = "All")
-top10_lift_dep <- full_rules_df_dependent %>% 
-  arrange(desc(lift)) %>% 
-  head(10) %>% 
-  mutate(Strategy = "top10.Lift", Set = "Dependent") %>% 
-  select(!Dependence)
-
-##
-all_options_df <-
-conf.90_all %>% 
-  rbind(conf.90_dep) %>% 
-  rbind(conf.100_all) %>% 
-  rbind(conf.100_dep) %>% 
-  rbind(lift.1_all) %>% 
-  rbind(lift.1_dep) %>% 
-  rbind(top100_conf_all) %>% 
-  rbind(top100_conf_dep) %>% 
-  rbind(top10_conf_all) %>% 
-  rbind(top10_conf_dep) %>% 
-  rbind(top100_conv_all) %>% 
-  rbind(top100_conv_dep) %>% 
-  rbind(top10_conv_all) %>% 
-  rbind(top10_conv_dep) %>% 
-  rbind(top100_lift_all) %>% 
-  rbind(top100_lift_dep) %>% 
-  rbind(top10_lift_all) %>% 
-  rbind(top10_lift_dep)
+# After threshold, if they are a lot, arrange by some other metric
 
 ## Summarise 
 
